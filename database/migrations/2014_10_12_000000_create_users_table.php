8b84f3b9932acc3e7292eca53531e3248f8bb2c0
TAGHREEDAA <taghreedMKamel@gmail.com>
2018-10-27 13:33:31 +0200

    modified users migrations added isStudent flad

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -21,6 +21,7 @@ class CreateUsersTable extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->boolean('isAdmin')->default(false);
+            $table->boolean('isStudent')->default(true);
             $table->rememberToken();
             $table->timestamps();
         });
